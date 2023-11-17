257d9ff03d931175697e4b0ca7bb4338a561cb10
Tuba <brunogabrielsd@hotmail.com>
2023-11-16 21:06:00 -0300

    feat: add infinite scroll for fetching reddit data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { GifListComponent } from "./ui/gif-list.component";
 import { RedditService } from '../shared/data-access/reddit.service';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    template: `<app-gif-list [gifs]="redditService.gifs()" class="grid-container" />`,
+    template: `
+      <app-gif-list 
+        [gifs]="redditService.gifs()"
+        infiniteScroll
+        (scrolled)="redditService.pagination$.next(redditService.lastKnownGif())"
+        class="grid-container" 
+      />`,
     styles: ``,
-    imports: [GifListComponent]
+    imports: [GifListComponent, InfiniteScrollModule]
 })
 export default class HomeComponent {
   redditService = inject(RedditService);
